be02185e9f0549cf4f830bf3ccb7bafe38152ddb
AGYAPAL SINGH <107750888+AgyapalSingh@users.noreply.github.com>
2023-08-07 15:57:24 +0530

    Recovey, Password, Reset UI created

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,15 +1,47 @@
 import toast from "react-hot-toast";
 
-// Validate Login page Username
-
+// 1. Validate Login page Username
 export async function usernameValidate(values) {
   const errors = usernameVerify({}, values);
+  return errors;
+}
+
+// 2.  Validate Password
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+  return errors;
+}
+
+// 3. Validate Reset password
+export async function resetPasswordValidation(values) {
+  const errors = passwordVerify({}, values);
+
+  if (values.password !== values.confirm_pwd) {
+    errors.exist = toast.error("Password not Match!");
+  }
+
+  return errors;
+}
+
+// Validate Password
+function passwordVerify(errors = {}, values) {
+  const specialChars = /[`!@#$%^&*()_+\-={};':"|,.<>?~]/;
+  if (!values.password) {
+    errors.password = toast.error("Password Required");
+  } else if (values.password.includes(" ")) {
+    errors.password = toast.error("Wrong Password");
+  } else if (values.password.length < 4) {
+    errors.password = toast.error(
+      "Password must be more than 4 character long"
+    );
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error("Password must have special characters");
+  }
 
   return errors;
 }
 
 // Validate username
-
 function usernameVerify(error = {}, values) {
   if (!values.username) {
     error.username = toast.error("Username Required");
