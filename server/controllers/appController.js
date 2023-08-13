c1dc90cf4acbf23b313963872a59edf3c64d7b24
AGYAPAL SINGH <107750888+AgyapalSingh@users.noreply.github.com>
2023-08-13 14:09:50 +0530

    GetUser Controller created

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -71,9 +71,6 @@ export async function login(req, res) {
             if (!passwordCheck)
               return res.status(400).send({ error: "Don't have Password" });
 
-            console.log("Stored hashed password:", user.password);
-            console.log("Password check result:", passwordCheck);
-
             // create jwt token
             const token = jwt.sign(
               {
@@ -102,48 +99,28 @@ export async function login(req, res) {
   }
 }
 
-// export async function login(req, res) {
-//   const { username, password } = req.body;
-
-//   try {
-//     const user = await UserModel.findOne({ username });
-
-//     if (!user) {
-//       return res.status(404).send({ error: "Username not Found" });
-//     }
-
-//     console.log("Stored hashed password:", user.password);
-
-//     const passwordCheck = await bcrypt.compare(password, user.password);
-
-//     console.log("Password check result:", passwordCheck);
-
-//     if (!passwordCheck) {
-//       return res.status(400).send({ error: "Invalid Username or Password" });
-//     }
-
-//     const token = jwt.sign(
-//       {
-//         userId: user._id,
-//         username: user.username,
-//       },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "24h" }
-//     );
-
-//     return res.status(200).send({
-//       msg: "Login Successful...!",
-//       username: user.username,
-//       token,
-//     });
-//   } catch (error) {
-//     return res.status(500).send({ error: "An error occurred" });
-//   }
-// }
-
 /** GET: http://localhost:8080/api/user/example123 */
 export async function getUser(req, res) {
-  res.json("getUser route");
+  const { username } = req.params;
+
+  try {
+    if (!username) {
+      return res.status(400).send({ error: "Invalid Username" });
+    }
+
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    // Remove sensitive data from the user object
+    const { password, ...userData } = user.toObject();
+
+    return res.status(200).send(userData);
+  } catch (error) {
+    return res.status(500).send({ error: "An error occurred" });
+  }
 }
 
 /** PUT: http://localhost:8080/api/updateuser 
